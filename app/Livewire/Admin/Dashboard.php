cc423458971c1ed1d5a9d32f58b9c67f5b1d86a4
Md Imran Hossain <mdimran24hrs@gmail.com>
2025-10-23 09:22:15 +0600

    dashboard data show done

--- app/Livewire/Admin/Dashboard.php
+++ app/Livewire/Admin/Dashboard.php
@@ -3,9 +3,46 @@
 namespace App\Livewire\Admin;
 
 use Livewire\Component;
+use App\Models\Policy;
+use App\Models\Service;
+use App\Models\Programme;
+use App\Models\Event;
+use App\Models\Partnership;
+use App\Models\Facility;
+use App\Models\Goal;
+use App\Models\Research;
+use App\Models\Report;
+use App\Models\News;
 
 class Dashboard extends Component
 {
+    public $pageTitle = "Dashboard";
+    public $total_goals;
+    public $total_policies;
+    public $total_services;
+    public $total_programmes;
+    public $total_events;
+    public $total_partnerships;
+    public $total_facilities;
+    public $total_research;
+    public $total_reports;
+    public $total_news;
+
+    public function mount()
+    {
+        $this->total_goals = Goal::where('status', STATUS_ACTIVE)->count();
+        $this->total_policies = Policy::where('status', STATUS_ACTIVE)->count();
+        $this->total_services = Service::where('status', STATUS_ACTIVE)->count();
+        $this->total_programmes = Programme::where('status', STATUS_ACTIVE)->count();
+        $this->total_events = Event::where('status', STATUS_ACTIVE)->count();
+        $this->total_partnerships = Partnership::where('status', STATUS_ACTIVE)->count();
+        $this->total_facilities = Facility::where('status', STATUS_ACTIVE)->count();
+        $this->total_research = Research::where('status', STATUS_ACTIVE)->count();
+        $this->total_reports = Report::where('status', STATUS_ACTIVE)->count();
+        $this->total_news = News::where('status', STATUS_ACTIVE)->count();
+    }
+
+
     public function render()
     {
         return view('livewire.admin.dashboard');
--- resources/views/livewire/admin/dashboard.blade.php
+++ resources/views/livewire/admin/dashboard.blade.php
@@ -1,3 +1,188 @@
 <div>
-    
+    <!-- Page Title Start -->
+    <div class="flex items-center justify-between mb-6">
+        <h4 class="text-slate-900 dark:text-slate-200 text-lg font-medium">{{ $pageTitle ?? '' }}</h4>
+
+    </div>
+    <!-- Page Title End -->
+    <div class="grid lg:grid-cols-4 md:grid-cols-2 gap-6 mb-6">
+        <div class="col-span-1">
+            <div class="card">
+                <div class="p-6">
+                    <div class="flex items-center">
+                        <div class="flex-shrink-0 me-3">
+                            <div class="w-12 h-12 flex justify-center items-center rounded text-primary bg-primary/25">
+                                <i class="mgc_building_2_line text-xl"></i>
+                            </div>
+                        </div>
+                        <div class="flex-grow">
+                            <h5 class="mb-1">{{ __("Active Goals") }}</h5>
+                            <p>{{ $total_goals }}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-span-1">
+            <div class="card">
+                <div class="p-6">
+                    <div class="flex items-center">
+                        <div class="flex-shrink-0 me-3">
+                            <div class="w-12 h-12 flex justify-center items-center rounded text-success bg-success/25">
+                                <i class="mgc_document_line text-xl"></i>
+                            </div>
+                        </div>
+                        <div class="flex-grow">
+                            <h5 class="mb-1">{{ __("Active Policy") }}</h5>
+                            <p>{{ $total_policies }}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-span-1">
+            <div class="card">
+                <div class="p-6">
+                    <div class="flex items-center">
+                        <div class="flex-shrink-0 me-3">
+                            <div class="w-12 h-12 flex justify-center items-center rounded text-info bg-info/25">
+                                <i class="mgc_server_line text-xl"></i>
+                            </div>
+                        </div>
+                        <div class="flex-grow">
+                            <h5 class="mb-1">{{ __("Active Services") }}</h5>
+                            <p>{{ $total_services }}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-span-1">
+            <div class="card">
+                <div class="p-6">
+                    <div class="flex items-center">
+                        <div class="flex-shrink-0 me-3">
+                            <div class="w-12 h-12 flex justify-center items-center rounded text-warning bg-warning/25">
+                                <i class="mgc_calendar_line text-xl"></i>
+                            </div>
+                        </div>
+                        <div class="flex-grow">
+                            <h5 class="mb-1">{{ __("Active Program") }}</h5>
+                            <p>{{ $total_programmes }}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+               <div class="col-span-1">
+            <div class="card">
+                <div class="p-6">
+                    <div class="flex items-center">
+                        <div class="flex-shrink-0 me-3">
+                            <div class="w-12 h-12 flex justify-center items-center rounded text-primary bg-primary/25">
+                                <i class="mgc_building_2_line text-xl"></i>
+                            </div>
+                        </div>
+                        <div class="flex-grow">
+                            <h5 class="mb-1">{{ __("Active Facilities") }}</h5>
+                            <p>{{ $total_facilities }}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-span-1">
+            <div class="card">
+                <div class="p-6">
+                    <div class="flex items-center">
+                        <div class="flex-shrink-0 me-3">
+                            <div class="w-12 h-12 flex justify-center items-center rounded text-success bg-success/25">
+                                <i class="mgc_clock_2_line text-xl"></i>
+                            </div>
+                        </div>
+                        <div class="flex-grow">
+                            <h5 class="mb-1">{{ __("Active Events") }}</h5>
+                            <p>{{ $total_events }}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-span-1">
+            <div class="card">
+                <div class="p-6">
+                    <div class="flex items-center">
+                        <div class="flex-shrink-0 me-3">
+                            <div class="w-12 h-12 flex justify-center items-center rounded text-info bg-info/25">
+                                <i class="mgc_search_line text-xl"></i>
+                            </div>
+                        </div>
+                        <div class="flex-grow">
+                            <h5 class="mb-1">{{ __("Active Research") }}</h5>
+                            <p>{{ $total_research }}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-span-1">
+            <div class="card">
+                <div class="p-6">
+                    <div class="flex items-center">
+                        <div class="flex-shrink-0 me-3">
+                            <div class="w-12 h-12 flex justify-center items-center rounded text-warning bg-warning/25">
+                                <i class="mgc_report_line text-xl"></i>
+                            </div>
+                        </div>
+                        <div class="flex-grow">
+                            <h5 class="mb-1">{{ __("Active Report") }}</h5>
+                            <p>{{ $total_reports }}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+               <div class="col-span-1">
+            <div class="card">
+                <div class="p-6">
+                    <div class="flex items-center">
+                        <div class="flex-shrink-0 me-3">
+                            <div class="w-12 h-12 flex justify-center items-center rounded text-info bg-info/25">
+                                <i class="mgc_news_line text-xl"></i>
+                            </div>
+                        </div>
+                        <div class="flex-grow">
+                            <h5 class="mb-1">{{ __("Active News") }}</h5>
+                            <p>{{ $total_news }}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-span-1">
+            <div class="card">
+                <div class="p-6">
+                    <div class="flex items-center">
+                        <div class="flex-shrink-0 me-3">
+                            <div class="w-12 h-12 flex justify-center items-center rounded text-warning bg-warning/25">
+                                <i class="mgc_user_5_line text-xl"></i>
+                            </div>
+                        </div>
+                        <div class="flex-grow">
+                            <h5 class="mb-1">{{ __("Active Partnerships") }}</h5>
+                            <p>{{ $total_partnerships }}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </div>
